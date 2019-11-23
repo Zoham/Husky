ce7bd5245074e350250a05ae42fc8a9f32566267
Soham Kulkarni <sohamk97@gmail.com>
2019-11-23 12:11:19 +0530

    Temporarily remove ngrok

--- Husky.py
+++ Husky.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from flask_ngrok import run_with_ngrok
+#from flask_ngrok import run_with_ngrok
 from flask import render_template
 
 app = Flask(__name__)
-run_with_ngrok(app)  # Start ngrok when app is run
+#run_with_ngrok(app)  # Start ngrok when app is run
 
 @app.route("/")
 def hello():
