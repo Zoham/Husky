1e6a8f6ec42ab946cb2d16d7664b3850ca410e49
Soham Kulkarni <sohamk97@gmail.com>
2019-11-30 12:44:10 +0530

    ajax and rasa linked (#4)
    Linked Ajax to Flask
    Linked up Rasa but returning parse results instead of response
    Refactors needed - taken in as technical debt

--- Husky.py
+++ Husky.py
@@ -1,13 +1,25 @@
 from flask import Flask
-#from flask_ngrok import run_with_ngrok
 from flask import render_template
+from flask import request, jsonify
+
+from rasa.nlu.model import Interpreter
 
 app = Flask(__name__)
-#run_with_ngrok(app)  # Start ngrok when app is run
+interpreter = Interpreter.load("./models/nlu")
 
 @app.route("/")
 def hello():
     return render_template("index.html")
+
+@app.route("/_add_numbers")
+def add_numbers():
+    query = request.args.get("query", "0", type=str)
+    return jsonify(result=askRasa(query))
+
+def askRasa(query):
+	reply = interpreter.parse(query)
+	print(reply)
+	return reply
 
 if __name__ == '__main__':
     app.run()
